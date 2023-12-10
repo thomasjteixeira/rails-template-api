4c050cf38786bab2d82b0a9afe6ae5988a0c357d
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-12-10 19:01:22 -0300

    :test_tube: [test] add validation tests for User model and shoulda_matchers configuration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,47 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { FactoryBot.build(:user) }
+
+  describe 'when all fields are present' do
+    it { is_expected.to respond_to(:email) }
+    it { is_expected.to respond_to(:password) }
+    it { is_expected.to respond_to(:password_confirmation) }
+  end
+
+  describe 'validations' do
+    describe 'presence validations' do
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_presence_of(:password) }
+    end
+
+    describe 'email validations' do
+      it { is_expected.to allow_value('example@domain.com').for(:email) }
+      it { is_expected.not_to allow_value('example.domain.com').for(:email) }
+      it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    end
+
+    describe 'password validations' do
+      it { is_expected.to validate_confirmation_of(:password) }
+      it { is_expected.to validate_length_of(:password).is_at_least(6) }
+    end
+
+    describe 'when creating a user with an invalid email' do
+      let(:user_with_invalid_email) { FactoryBot.build(:user, email: 'invalid_email') }
+
+      it 'is not valid' do
+        expect(user_with_invalid_email).not_to be_valid
+      end
+    end
+
+    describe 'when creating a user without an email' do
+      let(:user_without_email) { FactoryBot.build(:user, email: nil) }
+
+      it 'is not valid' do
+        expect(user_without_email).not_to be_valid
+      end
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,11 +3,16 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
 require_relative '../config/environment'
+require 'shoulda/matchers'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+rails_support_path = Rails.root.join('spec', 'support', '**', '*.rb')
+Dir[rails_support_path].each { |file| require file }
 
 if ENV['RAILS_ENV'] == 'test'
   require 'simplecov'
